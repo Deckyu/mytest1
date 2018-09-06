2d53e747fee40b60e40b3e23f2e043d5242435b0
Deckyu <yuyang93921@163.com>
2018-09-06 20:59:44 +0800

    congxin05tijiao

---
+++ test/src/main/java/test2/TestYY5.java
@@ -0,0 +1,13 @@
+package test2;
+
+/**
+ * 描述
+ *
+ * @author: yuy
+ */
+public class TestYY5 {
+    public static void main(String[] args) {
+        System.out.println(555555);
+        System.out.println(55551111);
+    }
+}
