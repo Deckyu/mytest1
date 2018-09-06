c612992f0aebda5bf5b93c01c6b514ff8a3da875
Deckyu <yuyang93921@163.com>
2018-09-06 19:53:16 +0800

    第一次提交

---
+++ test/src/main/java/test1/TestJava1.java
@@ -0,0 +1,14 @@
+package test1;
+
+import sun.applet.Main;
+
+/**
+ * 描述
+ *
+ * @author: yuy
+ */
+public class TestJava1 {
+    public static void main(String[] args) {
+        System.out.println("111111111111111");
+    }
+}
---
+++ test/src/main/java/test1/TestJava2.java
@@ -0,0 +1,9 @@
+package test1;
+
+/**
+ * 描述
+ *
+ * @author: yuy
+ */
+public class TestJava2 {
+}
